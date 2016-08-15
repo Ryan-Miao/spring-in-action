79481b41ded6e243f38ae70dcef7899ae1e65d08
Ryan <woshimrf@126.com>
2016-08-15 21:27:11 +0800

    add aop

--- quick-start/src/main/java/com/test/hello/Application.java
+++ quick-start/src/main/java/com/test/hello/Application.java
@@ -1,20 +1,15 @@
 package com.test.hello;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.*;
 
 /**
  * Created by rmiao on 8/15/2016.
  */
 @Configuration
 @ComponentScan
+@EnableAspectJAutoProxy
 public class Application {
-    private static final Logger logger = LoggerFactory.getLogger(Application.class);
 
     @Bean
     MessageService mockMessageService(){
@@ -22,10 +17,8 @@ public class Application {
     }
 
     public static void main(String[] args){
-        logger.debug("start");
         ApplicationContext context = new AnnotationConfigApplicationContext(Application.class);
         MessagePrinter printer = context.getBean(MessagePrinter.class);
         printer.printMessage();
-        logger.debug("end");
     }
 }
--- quick-start/src/main/java/com/test/hello/Monitor.java
+++ quick-start/src/main/java/com/test/hello/Monitor.java
@@ -1,6 +1,11 @@
 package com.test.hello;
 
+import org.aspectj.lang.annotation.After;
 import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
 /**
@@ -9,6 +14,21 @@ import org.springframework.stereotype.Component;
 @Aspect
 @Component
 public class Monitor {
+
+    private final Logger logger = LoggerFactory.getLogger(Monitor.class);
+
+    @Pointcut("execution(* com.test.hello.MessagePrinter.printMessage())")
+    public void  message(){}
+
+    @Before(value = "message()")
+    public void pre(){
+        logger.info("before print.");
+    }
+
+    @After(value = "message()")
+    public void after(){
+        logger.info("after print.");
+    }
 
 
 }
